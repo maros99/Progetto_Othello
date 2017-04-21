cc3a0bc111f46ffeb7b035e56b44ba8d9c378db1
marostica.lorenzo <marostica.lorenzo@TAVE09.OSDBSESTO.local>
2017-04-21 13:44:51 +0200

    Classe per il far partire il server

---
+++ Othello_Marostica/src/Controller/Server_Start.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+/**
+ *
+ * @author marostica.lorenzo
+ */
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server_Start {
+
+    public void Connect() throws IOException {
+
+        ServerSocket ss = new ServerSocket(2250);
+        while (true) {
+            new Server_Thread(ss.accept()).start(); 
+            
+          /*viene creato un thread SOLO QUANDO un client si collega dopo il 3wayHandleShake
+            il server rimanda indietro il pacchetto e basta.
+            Nel frattempo lui aspetta sempre, creando sempre un'altro thread per connessione
+          */
+        }
+    }
+}
