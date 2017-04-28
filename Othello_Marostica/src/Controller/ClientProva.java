cf129e1b1b665d4377eda06b24572dcf31ac2993
marostica.lorenzo <marostica.lorenzo@TAVE09.OSDBSESTO.local>
2017-04-28 10:47:24 +0200

    Client per provare il server

--- Othello_Marostica/src/Controller/ClientProva.java
+++ Othello_Marostica/src/Controller/ClientProva.java
@@ -20,17 +20,14 @@ public class ClientProva {
 
     public ClientProva() {
         try {
-            Socket s = new Socket("192.168.56.1", 2250);
+            Socket s = new Socket("10.1.33.9", 2250);
             sock_in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             sock_out = new PrintWriter(new OutputStreamWriter(s.getOutputStream()), true);
             std_in = new BufferedReader(new InputStreamReader(System.in));
             while (true) {
             try {
                 sock_out.println(std_in.readLine());
-                sock_out.println("echo");
-                sock_out.println("echo2");
-                //std_out.println(sock_in.readLine());
-                System.out.println(sock_in.readLine());
+                sock_out.println("ECHO");
                 System.out.println(sock_in.readLine());
                 System.out.println(sock_in.readLine());
             } catch (SocketException e) {
