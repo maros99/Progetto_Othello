e64a5c2203be35ac870209fbdcc210aeca2e971b
marostica.lorenzo <marostica.lorenzo@TAVE09.OSDBSESTO.local>
2017-04-28 09:48:46 +0200

    Classe per la pedina

---
+++ Othello_Marostica/src/Model/Pedina.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author marostica.lorenzo
+ */
+public class Pedina {
+    
+    private boolean bianco = true;
+    public boolean visibility = false;
+    
+    public boolean getColor(){
+        return bianco;
+    }
+    
+}
