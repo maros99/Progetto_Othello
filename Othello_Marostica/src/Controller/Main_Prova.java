ebdcf3f86bec4d77ead0e04f1f73cc6b99485595
marostica.lorenzo <marostica.lorenzo@TAVE09.OSDBSESTO.local>
2017-04-28 13:07:01 +0200

    Main di prova

--- Othello_Marostica/src/Controller/Main_Prova.java
+++ Othello_Marostica/src/Controller/Main_Prova.java
@@ -13,7 +13,6 @@ import controller.ClientProva;
  * @author marostica.lorenzo
  */
 public class Main_Prova {
-
     
     public static void main(String[] args) {
         Server_Start s = new Server_Start();    
