2dbeb1e2a91d058d21d7434bc1094c34a1388bca
marostica.lorenzo <marostica.lorenzo@TAVE09.OSDBSESTO.local>
2017-05-16 12:23:10 +0200

    Classe pedina da sostituire con \cella oppure mod. \Mappa

--- Othello_Marostica/src/Model/Pedina.java
+++ Othello_Marostica/src/Model/Pedina.java
@@ -1,15 +1,15 @@
 package Model;
+
 public class Pedina {
-     private boolean colore;
-     private int colonna;
-     private int riga;
-     public Pedina(int r,int c, boolean player){
-     r = riga;
-     c = colonna;
-     player = colore;
-     }
 
-       
-   }
+    private boolean colore;
+    private int colonna;
+    private int riga;
 
+    public Pedina(int r, int c, boolean player) {
+        r = riga;
+        c = colonna;
+        player = colore;
+    }
 
+}
