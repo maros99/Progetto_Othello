8235c5cfc6f95acc7b8ae839257b188c03509591
diallo.thiernooumaro <diallo.thiernooumaro@TAVE10.OSDBSESTO.local>
2017-06-01 01:38:22 +0200

    direzione diventa enum

--- Othello_Marostica/src/Model/Direzione.java
+++ Othello_Marostica/src/Model/Direzione.java
@@ -1,24 +1,41 @@
 package Model;
-public class Direzione {
-
-    public int driga;
-     public int dcolonna;
-     
+public enum Direzione {
+   UP_LEFT, UP , UP_RIGHT, RIGHT, DOWN_RIGHT, DOWN, DOWN_LEFT, LEFT;
     /**
      * Costruttore
      * I parametri passati vengono assenati alle proprietà della classe 
      * @param r
      * @param c
      */
-    public Direzione(int r, int c){
-        driga = r;
-        dcolonna =c;
-    }
-    /**
+
+      public Pedina next(Pedina epn){
+        switch(this){
+            case UP_LEFT:
+                return new Pedina(epn.riga -1, epn.colonna -1);
+            case DOWN: 
+                return new Pedina(epn.riga , epn.colonna +1);
+            case RIGHT:
+                return new Pedina(epn.riga , epn.colonna +1);
+            case UP: 
+                  return new Pedina(epn.riga , epn.colonna -1);
+            case LEFT:
+                return new Pedina(epn.riga -1, epn.colonna );
+            case UP_RIGHT:
+                return new Pedina(epn.riga +1, epn.colonna -1);
+            case DOWN_LEFT:
+                return new Pedina(epn.riga -1, epn.colonna +1);
+            case DOWN_RIGHT:
+                return new Pedina(epn.riga +1, epn.colonna +1);
+              
+        }
+           return new Pedina(epn.riga,epn.colonna);
+   }
+}
+/**
      * Meotodo per gesire movimento delle pedine
      * @return
      */
-    public String MostraDirezione(){
+    /*public String MostraDirezione(){
         if(driga == -1 && dcolonna == 0){
             return "Su";
         }
@@ -52,4 +69,4 @@ public class Direzione {
             else    
                 return "{ " + driga + ";" + dcolonna + "}";
     }
-}
+}*/
